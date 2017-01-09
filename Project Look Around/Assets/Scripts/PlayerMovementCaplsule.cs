b425789aaa6c9c389700da2d5f1c43bba1f9afe2
Nevelor <valerian-wolff@web.de>
2017-01-09 01:39:02 +0100

    added scripts | folder management

---
+++ Project Look Around/Assets/Scripts/PlayerMovementCaplsule.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementCaplsule : MonoBehaviour {
+
+    public float speed = 10f;
+    public float jumpForce = 8f;
+    public float gravity = 30f;
+    private Vector3 moveDir = Vector3.zero;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        CharacterController controller = gameObject.GetComponent<CharacterController>();
+
+        if (controller.isGrounded)
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDir = transform.TransformDirection(moveDir);
+            moveDir *= speed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDir.y = jumpForce;
+            }
+        }
+
+        moveDir.y -= gravity * Time.deltaTime;
+
+        controller.Move(moveDir * Time.deltaTime);
+
+    }
+}
