6415ff383cf0c5c83f0405fe000c9004c4a5e8ba
Nevelor <valerian-wolff@web.de>
2017-01-09 02:51:17 +0100

    tutorial

---
+++ Project Look Around/Assets/Scripts/Finishline.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Finishline : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadSceneAsync("MainMenue", LoadSceneMode.Additive);
+    }
+}
