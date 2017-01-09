b6ce67c2e445683ccbab852f7639edaad6e842ad
Nevelor <valerian-wolff@web.de>
2017-01-09 08:46:28 +0100

    parkour erstellt | smooth camera added
    spieler muss rotieren können um sich um zu sehen

---
+++ Project Look Around/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class CameraController : MonoBehaviour {
+
+    public float MoveSpeed;
+    public float RotationSpeed;
+    CharacterController charContr;
+
+	
+	void Start () {
+        charContr = GetComponent<CharacterController>();
+	}
+	
+	
+	void Update () {
+        Vector3 forward = Input.GetAxis("Vertical") * transform.TransformDirection(Vector3.forward) * MoveSpeed;
+        transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * RotationSpeed * Time.deltaTime, 0));
+        charContr.Move(forward * Time.deltaTime);
+        charContr.SimpleMove(Physics.gravity);
+	}
+}
---
+++ Project Look Around/Assets/SphereMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMovement : MonoBehaviour {
+
+    public float moveSpeed = 5.0f;
+    public float drag = 0.5f;
+    public float terminalRotationSpeed = 25.0f;
+    public Vector3 MoveVector { set; get; }
+    
+
+    private Rigidbody thisRigidbody;
+    private Transform camTransform;
+
+
+    void Start () {
+        thisRigidbody = gameObject.AddComponent<Rigidbody>();
+        thisRigidbody.maxAngularVelocity = terminalRotationSpeed;
+        thisRigidbody.drag = drag;
+	}
+	
+	
+	void Update () {
+        MoveVector = PoolInput();
+        Move();
+	}
+
+    private void Move()
+    {
+        thisRigidbody.AddForce((MoveVector * moveSpeed));
+    }
+
+    private Vector3 PoolInput()
+    {
+        Vector3 dir = Vector3.zero;
+
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+
+        if (dir.magnitude > 1)
+            dir.Normalize();
+
+        return dir;
+    }
+}
