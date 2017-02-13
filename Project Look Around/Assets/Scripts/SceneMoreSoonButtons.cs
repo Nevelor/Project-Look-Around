f2eed18770f46f0d7cdc9836184f82d0138022e0
DomeBetzler <Dominik.Betzler.online@gmx.de>
2017-02-13 11:17:05 +0100

    prefab fix, Canvas scaling, scripts

---
+++ Project Look Around/Assets/Scripts/SceneMoreSoonButtons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Class for the buttons
+/// in the scene MoreSoon
+/// Dominik Betzler
+/// </summary>
+public class SceneMoreSoonButtons : MonoBehaviour
+{
+    /// <summary>
+    /// loads the scene MainMenu
+    /// </summary>
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+	
+}
---
+++ Project Look Around/Assets/Scripts/SteuerungAnfang.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class to show the control pattern
+/// when you start the tutorial
+/// Dominik Betzler
+/// </summary>
+public class SteuerungAnfang : MonoBehaviour
+{
+    /// <summary>
+    /// gets the canvas
+    /// </summary>
+    public Canvas control;
+
+	/// <summary>
+    /// Sets timeScale to 0
+    /// so nothing else can be
+    /// triggerd
+    /// </summary>
+	void Start ()
+    {
+        Time.timeScale = 0;
+        control.enabled = true;
+	}
+	
+    /// <summary>
+    /// method for the button
+    /// to close the canvas
+    /// </summary>
+    public void BackToGame()
+    {
+        Time.timeScale = 1;
+        control.enabled = false;
+    }
+	
+}
